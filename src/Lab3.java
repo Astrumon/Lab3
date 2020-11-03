bdff76ddd716b2b3c05fc3e6764d0617376df755
Astrumon <dnlbdnk@gmail.com>
2020-11-03 22:24:54 +0200

    initial project version

---
+++ src/Lab3.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Lab3 {
+    public static void main(String[] args) {
+       /* Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+        Scanner scanner1 = new Scanner(System.in);
+        String text = scanner1.nextLine();
+        System.out.println(text);
+        scanner1.close();
+        scanner.close();*/
+
+
+        File file = new File("C:\\QuickAccess\\Test\\test.txt");
+
+      /*  try {
+            file.createNewFile();
+        } catch (IOException exc) {
+            System.out.println(exc);
+        } */
+
+        Date date = new Date(file.lastModified());
+        System.out.println("Последняя модификация: " + date);
+
+
+
+
+    }
+}
