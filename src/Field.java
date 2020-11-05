d8830c5df15bd78e2af9c22e0c4e25055cd22cad
Astrumon <dnlbdnk@gmail.com>
2020-11-05 12:47:48 +0200

    v3 Field

--- src/Field.java
+++ src/Field.java
@@ -2,9 +2,29 @@ public class Field {
 
     private static final int SIZE_ARRAY = 12;
     private String[][] arrayField = new String[SIZE_ARRAY][SIZE_ARRAY];
+    private static final String VALUE_HIDE = "#";
 
     public Field() {
-        String abc;
+        String defaultValue = "*";
+        initializeField(defaultValue);
+    }
+
+    public void showField() {
+        for (int i = 0; i < SIZE_ARRAY; i++) {
+            for (int j = 0; j < arrayField[i].length; j++) {
+                System.out.print(arrayField[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public void hideBattleField() {
+        initializeField(VALUE_HIDE);
+    }
+
+    public void initializeField(String value) {
         for (int i = 0; i < SIZE_ARRAY; i++) {
             for (int j = 0; j < arrayField[i].length; j++) {
                 if (i != SIZE_ARRAY - 1 && j != SIZE_ARRAY - 1) {
@@ -16,18 +36,9 @@ public class Field {
                 } else if (j == SIZE_ARRAY - 1) {
                     arrayField[i][j] = "|";
                 } else {
-                    arrayField[i][j] = "*";
+                    arrayField[i][j] = value;
                 }
             }
-        }
-    }
-
-    public void showField() {
-        for (int i = 0; i < SIZE_ARRAY; i++) {
-            for (int j = 0; j < arrayField[i].length; j++) {
-                System.out.print(arrayField[i][j] + "\t");
-            }
-            System.out.println();
         }
     }
 
--- src/Lab3.java
+++ src/Lab3.java
@@ -1,13 +1,10 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Date;
-import java.util.Scanner;
-
 public class Lab3 {
     public static void main(String[] args) {
-     Field field = new Field();
-    field.showField();
+        Field field = new Field();
+        field.showField();
+
+        field.hideBattleField();
+        field.showField();
 
 
     }
