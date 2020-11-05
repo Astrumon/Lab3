246e886449254b2d637f645b122cbc56ff9e1753
Astrumon <dnlbdnk@gmail.com>
2020-11-05 12:32:53 +0200

    v2 Field

--- src/Field.java
+++ src/Field.java
@@ -4,35 +4,31 @@ public class Field {
     private String[][] arrayField = new String[SIZE_ARRAY][SIZE_ARRAY];
 
     public Field() {
+        String abc;
         for (int i = 0; i < SIZE_ARRAY; i++) {
             for (int j = 0; j < arrayField[i].length; j++) {
-               if (i == SIZE_ARRAY-1) {
-                   arrayField[i][j] = "_";
-               } else if(j == SIZE_ARRAY-1){
-                   arrayField[i][j] = "|";
-               } else {
-                   arrayField[i][j] = "*";
-               }
+                if (i != SIZE_ARRAY - 1 && j != SIZE_ARRAY - 1) {
+                    arrayField[i][0] = String.valueOf(i);
+                    arrayField[0][j] = String.valueOf((char) (j + 64));
+                }
+                if (i == SIZE_ARRAY - 1) {
+                    arrayField[i][j] = "_";
+                } else if (j == SIZE_ARRAY - 1) {
+                    arrayField[i][j] = "|";
+                } else {
+                    arrayField[i][j] = "*";
+                }
             }
         }
     }
 
     public void showField() {
-        char ch;
         for (int i = 0; i < SIZE_ARRAY; i++) {
             for (int j = 0; j < arrayField[i].length; j++) {
-                if (j == 0 && i != SIZE_ARRAY-1) {
-                    System.out.print(i + "\t");
-                } else {
-                    if ((i == 0) && (j != SIZE_ARRAY-1)) {
-                        ch = (char)(j+64);
-                        System.out.print(ch + "\t");
-                    } else {
-                        System.out.print(arrayField[i][j] + "\t");
-                    }
-                }
+                System.out.print(arrayField[i][j] + "\t");
             }
             System.out.println();
         }
     }
+
 }
